9520763f2f347d57bfa156530e8c22c219d17532
Bobby Uno <robert.y.uno@gmail.com>
2020-03-05 01:12:29 +0000

    added users data

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,6 +1,10 @@
+INSERT INTO users (name, email, password) VALUES ('Toru Uno', t.uno@uno.com,$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.);
+INSERT INTO users (name, email, password) VALUES ('Sharon Uno', s.uno@uno.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.);
+INSERT INTO users (name, email, password) VALUES ('Daniel Uno', d.uno@uno.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.);
+INSERT INTO users (name, email, password) VALUES ('Bobby Uno', b.uno@uno.com, $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.);
+
 INSERT INTO properties () VALUES ();
 
-INSERT INTO users () VALUES ();
 
 INSERT INTO reservations () VALUES ();
 
